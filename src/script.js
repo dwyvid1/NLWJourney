c0db4b1392166002f446e7672621d7c26772e53c
Deyvid Matheus <deyvid.nascimento0101@gmail.com>
2024-07-12 09:29:35 -0300

    update

--- src/script.js
+++ src/script.js
@@ -13,12 +13,14 @@ const formatador = (data) => {
   }
 }
 
+// object {}
 const atividade = {
   nome: "Almoço",
   data: new Date("2024-07-08 10:00"),
   finalizada: true
 }
 
+// lista, array, vetor []
 let atividades = [
   atividade,
   {
@@ -33,7 +35,11 @@ let atividades = [
   },
 ]
 
+// atividades = []
+
+// arrow function
 const criarItemDeAtividade = (atividade) => {
+
   let input = `
   <input 
   onchange="concluirAtividade(event)"
@@ -41,61 +47,59 @@ const criarItemDeAtividade = (atividade) => {
   type="checkbox" 
   `
 
-  if(atividade.finalizada) {
+  if (atividade.finalizada) {
     input += 'checked'
   }
- 
+
   input += '>'
 
   const formatar = formatador(atividade.data);
 
   return `
-  <div class="card-bg">
-    ${input}
+    <div class="card-bg">
+      ${input}
 
-    <div>
-      <svg class="active" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <div>
+        <svg class="active" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M7.50008 10L9.16675 11.6667L12.5001 8.33335M18.3334 10C18.3334 14.6024 14.6025 18.3334 10.0001 18.3334C5.39771 18.3334 1.66675 14.6024 1.66675 10C1.66675 5.39765 5.39771 1.66669 10.0001 1.66669C14.6025 1.66669 18.3334 5.39765 18.3334 10Z" stroke="#BEF264" style="stroke:#BEF264;stroke:color(display-p3 0.7451 0.9490 0.3922);stroke-opacity:1;" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"/>
       </svg>
 
       <svg class="inactive" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M8.41664 1.81836C9.46249 1.61597 10.5374 1.61597 11.5833 1.81836M11.5833 18.1817C10.5374 18.3841 9.46249 18.3841 8.41664 18.1817M14.6741 3.10086C15.5587 3.70022 16.3197 4.46409 16.9158 5.35086M1.8183 11.5834C1.6159 10.5375 1.6159 9.46255 1.8183 8.4167M16.8991 14.6742C16.2998 15.5588 15.5359 16.3198 14.6491 16.9159M18.1816 8.4167C18.384 9.46255 18.384 10.5375 18.1816 11.5834M3.1008 5.32586C3.70016 4.44131 4.46403 3.68026 5.3508 3.0842M5.3258 16.8992C4.44124 16.2998 3.6802 15.536 3.08414 14.6492" stroke="#A1A1AA" style="stroke:#A1A1AA;stroke:color(display-p3 0.6314 0.6314 0.6667);stroke-opacity:1;" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"/>
       </svg>
-
-
+      
       <span>${atividade.nome}</span>
-    </div>
-    
-    <time class="short">
+      </div>
+
+      <time class="short">
       ${formatar.dia.semana.curto}.
       ${formatar.dia.numerico} <br>
       ${formatar.hora}
-    </time>
-    
-    <time class="full">
-      ${formatar.dia.semana.longo}, 
+      </time>
+      <time class="full">${formatar.dia.semana.longo}, 
       dia ${formatar.dia.numerico}
       de ${formatar.mes} 
-      às ${formatar.hora}h 
-    </time>
-  </div>
-  `
+      às ${formatar.hora}h </time>
+    </div>
+    `
 }
+
 
 const atualizarListaDeAtividades = () => {
   const section = document.querySelector('section')
   section.innerHTML = ''
 
-  if(atividades.length == 0) {
+  // verificar se a minha lista está vazia
+  if (atividades.length == 0) {
     section.innerHTML = `<p>Nenhuma atividade cadastrada.</p>`
     return
   }
 
-  for(let atividade of atividades) {
+  for (let atividade of atividades) {
     section.innerHTML += criarItemDeAtividade(atividade)
   }
-
 }
+
 atualizarListaDeAtividades()
 
 const salvarAtividade = (event) => {
@@ -117,7 +121,7 @@ const salvarAtividade = (event) => {
     return atividade.data == novaAtividade.data
   })
 
-  if(atividadeExiste) {
+  if (atividadeExiste) {
     return alert('Dia/Hora não disponível')
   }
 
@@ -136,7 +140,7 @@ const criarDiasSelecao = () => {
 
   let diasSelecao = ''
 
-  for(let dia of dias) {
+  for (let dia of dias) {
     const formatar = formatador(dia)
     const diaFormatado = `
     ${formatar.dia.numerico} de 
@@ -148,8 +152,8 @@ const criarDiasSelecao = () => {
   }
 
   document
-  .querySelector('select[name="dia"]')
-  .innerHTML = diasSelecao
+    .querySelector('select[name="dia"]')
+    .innerHTML = diasSelecao
 
 }
 criarDiasSelecao()
@@ -158,7 +162,7 @@ criarDiasSelecao()
 const criarHorasSelecao = () => {
   let horasDisponiveis = ''
 
-  for(let i = 6; i < 23; i++) {
+  for (let i = 6; i < 23; i++) {
     const hora = String(i).padStart(2, '0')
     horasDisponiveis += `
     <option value="${hora}:00">${hora}:00</option>`
@@ -167,8 +171,8 @@ const criarHorasSelecao = () => {
   }
 
   document
-  .querySelector('select[name="hora"]')
-  .innerHTML = horasDisponiveis
+    .querySelector('select[name="hora"]')
+    .innerHTML = horasDisponiveis
 }
 criarHorasSelecao()
 
@@ -180,7 +184,7 @@ const concluirAtividade = (event) => {
     return atividade.data == dataDesteInput
   })
 
-  if(!atividade) {
+  if (!atividade) {
     return
   }
 
